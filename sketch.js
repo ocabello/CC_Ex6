e1b6d33ffe445fea0de49ccf8b935175b8f1d789
ocabello <ocabello2@gmail.com>
2016-11-30 00:41:30 -0500

    added comments and deleted unnecessary variables

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-var APIkey = "&api-key=72c17cbc6ade401a8b2a6a143be9e219";
-var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
-var query = "?critics-pick=Y&opening-date=2016-11-01;2016-11-30";        
+var APIkey = "&api-key=72c17cbc6ade401a8b2a6a143be9e219"; 
+var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";   //NYT moview reviews api
+var query = "?critics-pick=Y&opening-date=2016-11-01;2016-11-30";        //searched for the movies that came out this
+                                                                         //this month and had good critic reviews
 
 var data;
 
 var visual = [];
-var decFactor = -5;
 
 function preload() {
   data = loadJSON(url + query + APIkey);
@@ -14,9 +14,9 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
-  var movies = data.results;
-  for (var i = 0; i < movies.length; i++) {
-    visual[i] = new Visualization(random(0.1, 5), random(width), random(height));
+  var movies = data.results;   //load data into movie array
+  for (var i = 0; i < movies.length; i++) {                                       //create the same amount of instances of Visualization class as the
+    visual[i] = new Visualization(random(0.1, 5), random(width), random(height)); //number of critically aclaimed movies
   }
   //noLoop();
 }
@@ -24,9 +24,7 @@ function setup() {
 function draw() {
   background(200, 215, 150);
 
-  var movies = data.results
-
-  for (var i = 0; i < visual.length; i++) {
+  for (var i = 0; i < visual.length; i++) {        //call all class functions to create data visualization
     //console.log(visual.length);
     var rand = 0.05;
     var gravity = 0.1;
--- visualization.js
+++ visualization.js
@@ -1,40 +1,36 @@
-var Visualization = function (m, locX, locY) {
-  this.mass = m;
+var Visualization = function (size, locX, locY) {
+  this.size = size;
   this.speed = random(0.2, 2);
   this.velocity = 0;
-  this.acceleration = 0;
   this.x = locX;
   this.y = locY;
   this.c = color(random(40,255), 45, random(80,200), 100);
-  this.angle = 200;
-  this.scaling = 500;
-  this.decrease;
   
-  this.create = function() {
-    rect(this.x, this.y, this.mass*16, this.mass*15);
+  this.create = function() {                          //creates rectangle shape
+    rect(this.x, this.y, this.size*16, this.size*15);
   };
   
-  this.vertMotion = function(force) {
-    this.velocity += force;
+  this.vertMotion = function(factor) {             //vertical velocity of objects is determined by factor being passed to function
+    this.velocity += factor;
     this.velocity *= random(-2,2);
   };
   
-  this.update = function() {
+  this.update = function() {                          //updates vertical velocity by making the object move back and forth
     this.y += random(-this.velocity, this.velocity);
   };
   
-  this.horizMotion = function(factor) {
+  this.horizMotion = function() {               //determines the horizontal speed of the objects
     this.x += this.speed;
   };
   
-  this.group = function(locX1, locY1) {
+  this.group = function(locX1, locY1) {     //creates effect of all objects moving towards each other
     this.x1 = locX1;
     this.y1 = locY1;
     
     
-      if (this.x1 < this.x && this.y1 < this.y) {       //move the current circleGroup object in the opposite direction
-        this.x += -this.speed;                      //at a speed of 1
-        this.y += -this.speed;
+      if (this.x1 < this.x && this.y1 < this.y) {       //objects will move towards the point that is passed to the function.
+        this.x += -this.speed;                          //the direction in which the objects will move is relative to their
+        this.y += -this.speed;                          //current position in the canvas.
       } else if (this.x1 > this.x && this.y1 < this.y) {
         this.x += this.speed;
         this.y += -this.speed;
@@ -47,7 +43,7 @@ var Visualization = function (m, locX, locY) {
       }
   }
   
-  this.edges = function() {
+  this.edges = function() {       //makes sure all objects remain within the canvas
     if (this.x > width){
       this.x = width;
       this.x *= -1;
